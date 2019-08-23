534da8bb5f4bfe2db44acd3dcee32a8f61c8267c
firnazluztian <firnazluztian@gmail.com>
2019-08-23 12:27:57 +0800

    initial commit & added page skeletons

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// java script file
+
+function main() {
+
+}
+
+// onClick
+function onClickFacebook() {
+	alert("login with facebook feature is coming soon");
+}
+
+function onClickGmail() {
+	alert("login with Gmail feature is coming soon");
+}
+
+main();

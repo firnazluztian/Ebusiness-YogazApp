66e9fb82cdb131a2c01b88e2d3c4e6bf5ae38d66
Tianliangyi <45553457+Tianliangyi@users.noreply.github.com>
2019-09-26 01:08:48 +0800

    Update script.js
    control the nav bar

--- static/js/script.js
+++ static/js/script.js
@@ -13,4 +13,10 @@ function onClickGmail() {
 	alert("login with Gmail feature is coming soon");
 }
 
+$('li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
 main();
